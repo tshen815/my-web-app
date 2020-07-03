5f257c003116d4e37d8058c3387ba7eb4d9892b4
Tony <tshen815@gmail.com>
2020-07-03 15:52:30 -0700

    fix some div closing tag issues

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -42,59 +42,61 @@ export default class About extends Component {
               <div
                 className="col-md-6 col-mid-offset-3 col-md-pull-3 animate-box"
                 data-animate-effects="fadeInLeft"
-              ></div>
-              <span className="heading-meta">What Do I Do?</span>
-              <h2 className="colorlib-heading">
-                {" "}
-                Here are some of my expertise
-              </h2>
-            </div>
-          </div>
-          <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
-              <div className="services color-1">
-                <span className="icon">
-                  <i className="icon-bulb" />
-                </span>
-                <div className="desc">
-                  <h3>Web Development</h3>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Aspernatur minima, blanditiis natus alias quibu
-                  </p>
-                </div>
+              >
+                <span className="heading-meta">What Do I Do?</span>
+                <h2 className="colorlib-heading">
+                  {" "}
+                  Here are some of my expertise
+                </h2>
               </div>
             </div>
-            <div className="col-md-4 text-center animate-box">
-              <div className="services color-3">
-                <span className="icon">
-                  <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                  <h3>UX Design</h3>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Vel quaerat totam exercitationem minus est, suscipit nihil
-                    nostrum, obcaecati amet culpa rem nemo veritatis quidem quia
-                    inventore repudiandae consequatur eos ducimus.
-                  </p>
+
+            <div className="row row-pt-md">
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                  <span className="icon">
+                    <i className="icon-bulb" />
+                  </span>
+                  <div className="desc">
+                    <h3>Web Development</h3>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Aspernatur minima, blanditiis natus alias quibu
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-              <div className="services color-5">
-                <span className="icon">
-                  <i className="icon-data" />
-                </span>
-                <div className="desc">
-                  <h3>SEO</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Minus voluptas accusamus inventore quam quia magni itaque
-                    saepe, repudiandae reprehenderit consectetur quibusdam
-                    deserunt eligendi, sit tempora in recusandae! Sed, facere
-                    neque?
-                  </p>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                  <span className="icon">
+                    <i className="icon-phone3" />
+                  </span>
+                  <div className="desc">
+                    <h3>UX Design</h3>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Vel quaerat totam exercitationem minus est, suscipit nihil
+                      nostrum, obcaecati amet culpa rem nemo veritatis quidem
+                      quia inventore repudiandae consequatur eos ducimus.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                  <span className="icon">
+                    <i className="icon-data" />
+                  </span>
+                  <div className="desc">
+                    <h3>SEO</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Minus voluptas accusamus inventore quam quia magni itaque
+                      saepe, repudiandae reprehenderit consectetur quibusdam
+                      deserunt eligendi, sit tempora in recusandae! Sed, facere
+                      neque?
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
