50469c8f1449679db943912bb6968fc27aa9893a
Tony <tshen815@gmail.com>
2020-07-03 15:33:12 -0700

    add ux design section under area of expertise

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
   render() {
@@ -8,13 +8,26 @@ export default class About extends Component {
           <div className="color-lib-narrow-content">
             <div className="row">
               <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effects="fadeInLeft">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effects="fadeInLeft"
+                >
                   <div className="col-md-12">
                     <div className="about-desc">
                       <span className="heading-meta">About Me</span>
                       <h2 className="colorlib-heading">Who Am I?</h2>
-                      <p>Skilled software engineer experienced in developing full-stack Javascript applications emphasizing a modularized React front end. Focusing on engineering empathy along with designing better user experiences through emphatic and progressive thinking.</p>
-                      <p> Living a double life away from coding as a competitive kick boxer, and an at home chef for friends and family.</p>
+                      <p>
+                        Skilled software engineer experienced in developing
+                        full-stack Javascript applications emphasizing a
+                        modularized React front end. Focusing on engineering
+                        empathy along with designing better user experiences
+                        through emphatic and progressive thinking.
+                      </p>
+                      <p>
+                        {" "}
+                        Living a double life away from coding as a competitive
+                        kick boxer, and an at home chef for friends and family.
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -26,9 +39,15 @@ export default class About extends Component {
         <section className="colorlib-about">
           <div className="colorlib-narrow-content">
             <div className="row">
-              <div className="col-md-6 col-mid-offset-3 col-md-pull-3 animate-box" data-animate-effects="fadeInLeft"></div>
+              <div
+                className="col-md-6 col-mid-offset-3 col-md-pull-3 animate-box"
+                data-animate-effects="fadeInLeft"
+              ></div>
               <span className="heading-meta">What Do I Do?</span>
-              <h2 className="colorlib-heading"> Here are some of my expertise</h2>
+              <h2 className="colorlib-heading">
+                {" "}
+                Here are some of my expertise
+              </h2>
             </div>
           </div>
           <div className="row row-pt-md">
@@ -39,13 +58,32 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                   <h3>Web Development</h3>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minima, blanditiis natus alias quibu</p>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Aspernatur minima, blanditiis natus alias quibu
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+              <div className="services color-3">
+                <span className="icon">
+                  <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                  <h3>UX Design</h3>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Vel quaerat totam exercitationem minus est, suscipit nihil
+                    nostrum, obcaecati amet culpa rem nemo veritatis quidem quia
+                    inventore repudiandae consequatur eos ducimus.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </section>
       </div>
-    )
+    );
   }
 }
